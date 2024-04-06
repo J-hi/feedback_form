9277956d9c77593896f76be7727cc600db162b82
Muskan-Akodiya <165752555+Muskan-Akodiya@users.noreply.github.com>
2024-04-06 16:23:56 +0530

    Update form(main).py

--- form(main).py
+++ form(main).py
@@ -16,7 +16,7 @@ contact=input("contact number:")
 #
 enroll_id=input("enrollment no:")
 #
-school==input("school:")
+school=input("school:")
 if(school.isdigit()):
     print("invalid input")
     last_name=input("last name: ")
@@ -46,3 +46,19 @@ if(faculty.isdigit()):
 sem=input("semester:")
 
 #add feedback questions with input statement
+
+knowledge=input("Subject knowledge of the Course Lead is:")
+communication_skills=input("Communication skills of the Course Lead:")
+punctual=input("Does the Instructor punctually come on the time of lecture:")
+insights=input("Did you gain anything useful from the Course lead:")
+another_course=input("Would you like to have another course with the same Course Lead:")
+learning=input("What were your learning objectives from this course:")
+development=input("How did this course develop you professionally?:")
+activity=input("Which activities did you find most useful? Why?:")
+
+
+
+
+
+
+
